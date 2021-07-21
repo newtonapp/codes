c990ef6ccf7127662ada6de1266229eecf935e41
newtonapp <82835712+newtonapp@users.noreply.github.com>
2021-07-21 11:59:26 +0530

    Update vpc.tf

--- Terraform/vpc.tf/vpc.tf
+++ Terraform/vpc.tf/vpc.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIAWHA5G5WGHNSUNCY6"
-  secret_key = "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"
-}
 resource "aws_vpc" "blueprd" {
     cidr_block = var.BLUE_VPC_CIDR
 
